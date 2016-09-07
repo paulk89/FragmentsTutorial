f08270bb08eb3e7de10368181cff54e05de3c4c7
paulk89 <paul.kelly89@hotmail.co.uk>
2016-09-07 12:56:05 +0100

    initial commit

---
+++ app/src/main/java/com/example/sita/fragmentstutorial/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.sita.fragmentstutorial;
+
+//import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.content.res.Configuration;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // Log.d("daasd","Activty launched");
+//        setContentView(R.layout.activity_main);
+
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        Configuration configInfo = getResources().getConfiguration();
+
+        if(configInfo.orientation == Configuration.ORIENTATION_LANDSCAPE){
+            LandscapeFragment  landscapeFragment = new LandscapeFragment();
+            fragmentTransaction.replace(android.R.id.content, landscapeFragment);
+        }else{
+            PortraitFragment  portraitFragment = new PortraitFragment();
+            fragmentTransaction.replace(android.R.id.content, portraitFragment);
+        }
+
+        fragmentTransaction.commit();
+
+    }
+}
---
+++ app/src/main/java/com/example/sita/fragmentstutorial/PortraitFragment.java
@@ -0,0 +1,21 @@
+package com.example.sita.fragmentstutorial;
+
+//import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Paul on 07/09/2016.
+ */
+public class PortraitFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.portrait_fragment, container, false);
+    }
+}
